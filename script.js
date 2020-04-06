c8269a41b957a5710ee89e0322eacd8cc451aaca
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-04-05 21:56:59 -0700

    Added visual elements to index.html, having issues with query.

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("from script.js");
+//console.log("from script.js");
+const API_KEY = "0842050e641f34d5007ea84fdf7ef33e";
+
+
+
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+
+//api.openweathermap.org/data/2.5/weather?q={city name},{state}&appid={your api key}
+
+//api.openweathermap.org/data/2.5/weather?q={city name},{state},{country code}&appid={your api key}
+
+// function buildQuery(city, state){
+
+
+function buildQuery(city, state){
+    return "api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&appid=" + API_KEY;
+};
+
+
+function sendQuery(query_url){
+    $.ajax({
+        url: query_url,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })
+};
+
+
+$("#form").on("submit", function() {
+    //console.log("User clicked Submit.");
+
+    //console.log($("#city").val() + ", " + $("#state").val());
+    // console.log(buildQuery($("#city").val(), $("#state").val()));
+
+    sendQuery(buildQuery($("#city").val(), $("#state").val()));
+
+
+
+
+    event.preventDefault();
+
+});
