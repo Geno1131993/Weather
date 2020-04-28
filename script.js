5227dd1423fa9c28f37a841aab66fc00eb1f848f
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-04-27 19:00:18 -0700

    Weather api is now working -- however, having issues with Moment getting current date, so program can determine current time of day.

--- script.js
+++ script.js
@@ -2,18 +2,19 @@
 const API_KEY = "0842050e641f34d5007ea84fdf7ef33e";
 
 
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
 
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+// api.openweathermap.org/data/2.5/forecast?q={city name},{state}&appid={your api key}
 
-//api.openweathermap.org/data/2.5/weather?q={city name},{state}&appid={your api key}
+// api.openweathermap.org/data/2.5/forecast?q={city name},{state},{country code}&appid={your api key}
 
-//api.openweathermap.org/data/2.5/weather?q={city name},{state},{country code}&appid={your api key}
+
 
 // function buildQuery(city, state){
 
 
 function buildQuery(city, state){
-    return "api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&appid=" + API_KEY;
+    return "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "," + state + "&appid=" + API_KEY;
 };
 
 
@@ -22,22 +23,23 @@ function sendQuery(query_url){
         url: query_url,
         method: "GET"
     }).then(function(response) {
+        //console.log(response);
         console.log(response);
-    })
+        //console.log(response["weather"][0]["main"]);
+    });
 };
 
 
 $("#form").on("submit", function() {
-    //console.log("User clicked Submit.");
-
-    //console.log($("#city").val() + ", " + $("#state").val());
-    // console.log(buildQuery($("#city").val(), $("#state").val()));
-
     sendQuery(buildQuery($("#city").val(), $("#state").val()));
-
-
-
-
     event.preventDefault();
-
 });
+
+
+
+function addDays(){
+    
+
+
+}
+
