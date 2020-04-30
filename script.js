676ec48b8de26241ff4bb15723e4d0fe9355e8b6
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-04-29 21:20:47 -0700

    Clicking on the search elements aren't working correctly -- also, I chose not to include the UVI in the current weather panel, as it would include a lot of work to figure out the lat/lon of the city the user wants.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//console.log("from script.js");
 const API_KEY = "0842050e641f34d5007ea84fdf7ef33e";
-
 
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
 
@@ -9,8 +7,6 @@ const API_KEY = "0842050e641f34d5007ea84fdf7ef33e";
 // api.openweathermap.org/data/2.5/forecast?q={city name},{state},{country code}&appid={your api key}
 
 
-
-// function buildQuery(city, state){
 
 
 function buildQuery(city, state){
@@ -29,6 +25,8 @@ function sendQuery(query_url){
 };
 
 
+
+
 $("#form").on("submit", function() {
     sendQuery(buildQuery($("#city").val(), $("#state").val()));
     event.preventDefault();
@@ -36,15 +34,23 @@ $("#form").on("submit", function() {
 
 
 
+
+
+
 function addDays(list, city, state){
-    console.log(list);
+    let current = list[0]["main"];
 
 
     $("#forecast").html(`
         <nav class = "level">
             <div class = "level-left">
                 <div class = "level-item">
-
+                    <div id = "metrics">
+                        <div id = "location"> ${city}, ${state} <br></div>
+                        <div id = "temp"> Current temperature: ${toFarenheit(current["temp"])}F</div>
+                        <div id = "feels"> Feels like: ${toFarenheit(current["feels_like"])}F</div>
+                        <div id = "hum"> Humidity: ${current["humidity"]}%</div>
+                    </div>
                 </div>
             </div>
             <div class = "level-right">
@@ -54,9 +60,9 @@ function addDays(list, city, state){
             </div>
         </nav>`);
 
+
     let html = ``;
     for(let i = 8; i < list.length; i += 8){
-        console.log(list[i]);
         html += `
             <article class = "tile day is-child">
                 <p>${list[i]["dt_txt"].split(" ")[0]}</p>
@@ -65,16 +71,25 @@ function addDays(list, city, state){
         `;
     }
     
+
     $("#days").html(html);
 
     let newSearchItem = `
-        <div id = "${city}${state}" class = "search">
-            <p>${city}, ${state}</p>
-        </div>
+        <div id = "${city}${state}" class = "search">${city}, ${state}</div>
     `;
     $("#history").append(newSearchItem);
+}
 
+
+function toFarenheit(K){
+    return Math.floor((K - 273.15) * (9/5) + 32);
 }
 
 
 
+
+//Having issues with the buttons, for some reason -- it's not listening for clicks, nor is the "focus" working in the css.
+
+$(".search").on("click", function(){
+    console.log("User wants to check out previous history.");
+});
